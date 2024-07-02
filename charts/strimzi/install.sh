fc9c6395bd531db2c5e757ca371b2f8a172ca1c7
Neil Maderthaner <neil.maderthaner@lsd.co.za>
2024-07-02 09:57:11 +0200

    add resources to user and topic operators

--- charts/strimzi/install.sh
+++ charts/strimzi/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 openssl genrsa -out ca-key.pem 2048
-openssl req -new -key ca-key.pem -x509 -days 3650 -out ca.pem -subj "/C=US/ST=CA/L=MountainView/O=Confluent/OU=Operator/CN=LocalCA" -config sans.conf -extensions 'v3_req'
+openssl req -new -key ca-key.pem -x509 -days 3650 -out ca.pem -subj "/C=ZA/ST=GP/L=Johannesburg/O=io.strimzi/OU=Operator/CN=LocalCA" -config sans.conf -extensions 'v3_req'
 openssl x509 -in ca.pem -noout -text
 
 kubectl label node kind-worker accept-pod=lsdmesp-broker-0
