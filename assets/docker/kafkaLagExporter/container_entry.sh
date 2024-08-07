990df48e670976766458d80426f8a50eaac3aeb6
Rainer Schamm <rainer@lsdopen.io>
2024-08-07 15:21:45 +0200

    Clean up KafkaLagExporter

--- assets/docker/kafkaLagExporter/container_entry.sh
+++ assets/docker/kafkaLagExporter/container_entry.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Starting kafka lag exporter with JAVA_OPTS: $JAVA_OPTS and ARGS: $ARGS"
-exec /opt/bin/kafka-lag-exporter -Dconfig.file=/root/etc/application.conf -Dlogback.configurationFile=/opt/logback.xml
+exec /opt/bin/kafka-lag-exporter -Dconfig.file=/opt/etc/application.conf -Dlogback.configurationFile=/opt/logback.xml
 echo "Exiting shell..."
