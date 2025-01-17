e0eab098b95dc7e92200fa5c7fc7f42898073c47
Rainer Schamm <rainer@lsdopen.io>
2025-01-17 15:15:44 +0200

    Fixing CMF support up a bit

--- charts/confluent/install.sh
+++ charts/confluent/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+kubectl create -f https://github.com/jetstack/cert-manager/releases/download/v1.16.2/cert-manager.yaml
+echo "Sleeping a bit for cert-manager"
+sleep 10
+echo "Done!"
 helm dependency update .
 kubectl create ns lsdmesp
 kubectl config set-context --current --namespace lsdmesp
